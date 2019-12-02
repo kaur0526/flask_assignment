9f2d325c480dd1e236b4ceb23bb252b08bb9b89c
kaur0526 <54955041+kaur0526@users.noreply.github.com>
2019-12-01 22:56:13 -0500

    Update president.py

--- president.py
+++ president.py
@@ -1,1 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return '<h1>Welcome to the presidential Flask example!</h1>'
+
+# your code here
+
+
+
+
+# keep this as is
+if __name__ == '__main__':
+    app.run(debug=True)
 
